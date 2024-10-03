195c6dcfd68a0c2f71350cab656a5109f660db2c
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-10-03 18:39:08 +0800

    commented http functions out

--- Assets/Editor/AssetBundleToolsWindow.cs
+++ Assets/Editor/AssetBundleToolsWindow.cs
@@ -52,7 +52,8 @@ public class AssetBundleToolsWindow : EditorWindow
             else if (resourceServerAddress.Contains("http"))
             {
                 //UploadAllBundlesAndComparisonFileToHTTPServer();
-                DownloadFileFromHTTPServer("File.txt", "E:\\GameProject\\File.txt");
+                //DownloadFileFromHTTPServer("File.txt", "E:\\GameProject\\File.txt");
+                Debug.LogError("I didn't implement HTTP file upload function cuz i don't wanna write http server code!");
             }
         }
     }
@@ -108,92 +109,92 @@ public class AssetBundleToolsWindow : EditorWindow
     }
 
 
-    public void UploadAllBundlesAndComparisonFileToHTTPServer()
-    {
-        DirectoryInfo directory = Directory.CreateDirectory(Application.dataPath + $"\\ResourcesForHotFix\\AssetBundles\\{platforms[selectedPlatformIndex]}");
-        FileInfo[] fileInfos = directory.GetFiles();
+    //public void UploadAllBundlesAndComparisonFileToHTTPServer()
+    //{
+    //    DirectoryInfo directory = Directory.CreateDirectory(Application.dataPath + $"\\ResourcesForHotFix\\AssetBundles\\{platforms[selectedPlatformIndex]}");
+    //    FileInfo[] fileInfos = directory.GetFiles();
 
-        foreach (var fileInfo in fileInfos)
-        {
-            //empty extension is the asset bundle
-            //.txt extension is lua script
-            if (fileInfo.Extension == "" || fileInfo.Extension == ".txt")
-            {
-                //Debug.Log(fileInfo.Name);
-                UploadFileToHTTPServer(fileInfo.FullName, fileInfo.Name);
-            }
-        }
-    }
+    //    foreach (var fileInfo in fileInfos)
+    //    {
+    //        //empty extension is the asset bundle
+    //        //.txt extension is lua script
+    //        if (fileInfo.Extension == "" || fileInfo.Extension == ".txt")
+    //        {
+    //            //Debug.Log(fileInfo.Name);
+    //            UploadFileToHTTPServer(fileInfo.FullName, fileInfo.Name);
+    //        }
+    //    }
+    //}
 
-    private async void DownloadFileFromHTTPServer(string _fileName, string _downloadLocation)
-    {
-        await Task.Run(() =>
-        {
-            try
-            {
-                HttpWebRequest request = HttpWebRequest.Create(new Uri(resourceServerAddress + "/" + _fileName)) as HttpWebRequest;
-                request.Method = WebRequestMethods.Http.Get;
-                request.Proxy = null;
-                request.KeepAlive = false;
+    //private async void DownloadFileFromHTTPServer(string _fileName, string _downloadLocation)
+    //{
+    //    await Task.Run(() =>
+    //    {
+    //        try
+    //        {
+    //            HttpWebRequest request = HttpWebRequest.Create(new Uri(resourceServerAddress + "/" + _fileName)) as HttpWebRequest;
+    //            request.Method = WebRequestMethods.Http.Get;
+    //            request.Proxy = null;
+    //            request.KeepAlive = false;
 
-                var response = request.GetResponse();
-                Stream downloadStream = response.GetResponseStream();
+    //            var response = request.GetResponse();
+    //            Stream downloadStream = response.GetResponseStream();
 
-                using (FileStream fs = new FileStream(_downloadLocation, FileMode.Create))
-                {
-                    byte[] bytes = new byte[2048];
-                    int contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+    //            using (FileStream fs = new FileStream(_downloadLocation, FileMode.Create))
+    //            {
+    //                byte[] bytes = new byte[2048];
+    //                int contentLength = downloadStream.Read(bytes, 0, bytes.Length);
 
-                    while (contentLength > 0)
-                    {
-                        fs.Write(bytes, 0, contentLength);
+    //                while (contentLength > 0)
+    //                {
+    //                    fs.Write(bytes, 0, contentLength);
 
-                        contentLength = downloadStream.Read(bytes, 0, bytes.Length);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex.Message);
-            }
-        });
-    }
+    //                    contentLength = downloadStream.Read(bytes, 0, bytes.Length);
+    //                }
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Debug.LogError(ex.Message);
+    //        }
+    //    });
+    //}
 
-    private async void UploadFileToHTTPServer(string _filePath, string _fileName)
-    {
-        await Task.Run(() =>
-        {
-            try
-            {
-                HttpWebRequest request = HttpWebRequest.Create(new Uri($"{resourceServerAddress}/AssetBundles/{platforms[selectedPlatformIndex]}/" + _fileName)) as HttpWebRequest;
-                request.Method = WebRequestMethods.Http.Put;
-                request.Proxy = null;
-                request.KeepAlive = false;
+    //private async void UploadFileToHTTPServer(string _filePath, string _fileName)
+    //{
+    //    await Task.Run(() =>
+    //    {
+    //        try
+    //        {
+    //            HttpWebRequest request = HttpWebRequest.Create(new Uri($"{resourceServerAddress}/AssetBundles/{platforms[selectedPlatformIndex]}/" + _fileName)) as HttpWebRequest;
+    //            request.Method = WebRequestMethods.Http.Put;
+    //            request.Proxy = null;
+    //            request.KeepAlive = false;
 
-                Stream uploadStream = request.GetRequestStream();
+    //            Stream uploadStream = request.GetRequestStream();
 
-                using (FileStream fs = File.OpenRead(_filePath))
-                {
-                    byte[] buffer = new byte[2048];
-                    int contentLength = fs.Read(buffer, 0, buffer.Length);
+    //            using (FileStream fs = File.OpenRead(_filePath))
+    //            {
+    //                byte[] buffer = new byte[2048];
+    //                int contentLength = fs.Read(buffer, 0, buffer.Length);
 
-                    while (contentLength > 0)
-                    {
-                        uploadStream.Write(buffer, 0, contentLength);
+    //                while (contentLength > 0)
+    //                {
+    //                    uploadStream.Write(buffer, 0, contentLength);
 
-                        contentLength = fs.Read(buffer, 0, buffer.Length);
-                    }
-                }
+    //                    contentLength = fs.Read(buffer, 0, buffer.Length);
+    //                }
+    //            }
 
-                uploadStream.Close();
-                Debug.Log($"[{_fileName}] uploaded!");
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError(ex.Message);
-            }
-        });
-    }
+    //            uploadStream.Close();
+    //            Debug.Log($"[{_fileName}] uploaded!");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Debug.LogError(ex.Message);
+    //        }
+    //    });
+    //}
 
     private async void UploadFileToFTPServer(string _filePath, string _fileName)
     {
